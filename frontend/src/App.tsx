9f97854e59ffd9837de90e57ac41c0cdf0497877
Michael Tavyryn <mihailtavyrin2@gmail.com>
2026-02-12 19:21:24 +0300

    #1 add prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <>
-        <h1>Hello! It's React app with Vite</h1>
+      <h1>Hello! It's React app with Vite</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
