0535369c35dae3267823f4c4975785f9571b553d
michaeltavyrin <105814138+bssier@users.noreply.github.com>
2026-02-16 20:17:37 +0300

    #22, add mui (#2)
    Co-authored-by: Michael Tavyryn <mihailtavyrin2@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <>
-      <h1>Hello! It's React app with Vite</h1>
+      <Button variant="contained">Hello world</Button>
     </>
   );
 }
