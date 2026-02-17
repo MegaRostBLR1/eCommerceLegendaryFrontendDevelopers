719288cf7687d1dc648a43ed61b8eeb1e5c3d991
Rorodeathless1 <romochka-suvorov-2016@mail.ru>
2026-02-17 16:58:11 +0300

    feat: add main page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Button from '@mui/material/Button';
+import { HomePage } from './pages/home-page/home-page';
 
-function App() {
+export function App() {
   return (
     <>
+      < HomePage/>
       <Button variant="contained">Hello world</Button>
     </>
-  );
+  )
 }
 
-export default App;
+
---
+++ frontend/src/pages/home-page/home-page.tsx
@@ -0,0 +1,69 @@
+import { Logo } from '../../components/icons/logo';
+import './home-page.css';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Card } from './ui/card/card';
+
+const DATA = [
+  {
+    title: 'titl',
+    price: 100,
+    description: 'description',
+    id: 1,
+  },
+  {
+    title: 'tit',
+    price: 10,
+    description: 'description',
+    id: 2,
+  },
+  {
+    title: 'ti',
+    price: 10,
+    description: 'description',
+    id: 3,
+  },
+  {
+    title: 't',
+    price: 1000,
+    description:
+      'lorem ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis description description',
+    id: 4,
+  },
+];
+
+export function HomePage() {
+  return (
+    <main className="page-main">
+      <section className="title-section">
+        <div className="container">
+          <div className="title-wrapper">
+            <Logo className="title-logo" />
+            <h2 className="title-section-title">Услуги от ИИ</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className="bestseller">
+        <div className="container">
+          <div className="bestseller-header">
+            <h2 className="bestseller-title">Хит продаж</h2>
+            <a className="bestseller-link" href="/Каталог/услуги">
+              <span>Все улуги</span>
+              <ArrowForwardIosIcon className="bestseller-arrow-right" />
+            </a>
+          </div>
+          <div className="bestseller-wrapper">
+            {DATA.map((item) => (
+              <Card
+                key={item.id}
+                title={item.title}
+                price={item.price}
+                description={item.description}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
