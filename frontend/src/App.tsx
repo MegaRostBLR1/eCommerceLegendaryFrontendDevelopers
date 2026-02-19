81d7cf64ef3b1ca91062e01e23ccd3daf47d3f1b
Breslavets Maria <136121235+Morevna@users.noreply.github.com>
2026-02-19 20:59:47 +0300

    feat: #18, add header with nav (#5)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import './App.css';
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/header.tsx';
+import {Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage.tsx';
 import Footer from './components/Footer/Footer.tsx';
 
 export function App() {
   return (
-    <BrowserRouter>
+    <>
+      <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
+
+export default App;
