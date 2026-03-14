b0d6a3c87e68505d44f8e2c014a61ee3052667fe
Breslavets Maria <alwayslearn@mail.ru>
2026-03-14 20:47:15 +0300

    #45, fix bag with all categories

--- frontend/src/components/modals/AuthorizationModal/AuthorizationModal.tsx
+++ frontend/src/components/modals/AuthorizationModal/AuthorizationModal.tsx
@@ -20,9 +20,9 @@ import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const BASE_URL = environment.baseUrl;
 export default function OpenLoginModal({
-                                         open,
-                                         onClose,
-                                       }: {
+  open,
+  onClose,
+}: {
   open: boolean;
   onClose: () => void;
 }) {
