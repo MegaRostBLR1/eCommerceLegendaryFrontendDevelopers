16daa36e5234f973f7f1eb9a8cd8edef987f20d1
andrski <andrski2009@gmail.com>
2026-03-16 21:18:59 +0300

    add statistics, not finished

--- backend/src/routes/validators/validators.ts
+++ backend/src/routes/validators/validators.ts
@@ -1,6 +1,24 @@
 import { body, query } from 'express-validator';
+import { isDate } from 'lodash';
+import { HttpException } from '../../exceptions/exception';
 
 export const validators = {
     login: [body('email').isEmail().withMessage('Email required'), body('password').isString().isLength({ min: 3 }).withMessage('Password required')],
     search: [query('search').optional().isString().isLength({ min: 3 }).withMessage('Search minimum length is 3 symbols')],
+    dates: [
+        query('dateStart')
+            .isString()
+            .toDate()
+            .custom((value) => {
+                if (!(isDate(value) && !isNaN(value.getTime()))) throw new HttpException(400, 'Bad start date');
+                else return true;
+            }),
+        query('dateEnd')
+            .isString()
+            .toDate()
+            .custom((value) => {
+                if (!(isDate(value) && !isNaN(value.getTime()))) throw new HttpException(400, 'Bad end date');
+                else return true;
+            }),
+    ],
 };
